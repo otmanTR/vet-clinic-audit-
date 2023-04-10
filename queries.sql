c915e6d022c7634652fd40bd7d7f097a5b1726b4
otmanTR <r.brk.otman@gmail.com>
2023-04-10 16:35:01 +0300

    add ındex

--- queries.sql
+++ queries.sql
@@ -1,3 +1,4 @@
-/*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+    SELECT COUNT(*) FROM visits where animal_id = 4;
+    SELECT * FROM visits where vet_id = 2;
+    SELECT * FROM owners where email = 'owner_18327@mail.com';
